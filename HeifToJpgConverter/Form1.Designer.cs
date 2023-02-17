78f30ba16dc05bbe42e21071b4a871d611703ac4
Joffre Lustosa do Amaral Mota <joffre.mota@encontact.com.br>
2023-02-17 06:50:27 -0300

    Conversion implementation

---
+++ HeifToJpgConverter/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace HeifToJpgConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOriginFile = new System.Windows.Forms.TextBox();
+            this.btnSelectOriginFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDestinationFolder = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectDestinationFolder = new System.Windows.Forms.Button();
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.BtnInfo = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Origin File";
+            // 
+            // txtOriginFile
+            // 
+            this.txtOriginFile.Enabled = false;
+            this.txtOriginFile.Location = new System.Drawing.Point(6, 37);
+            this.txtOriginFile.Name = "txtOriginFile";
+            this.txtOriginFile.Size = new System.Drawing.Size(436, 23);
+            this.txtOriginFile.TabIndex = 1;
+            // 
+            // btnSelectOriginFile
+            // 
+            this.btnSelectOriginFile.Location = new System.Drawing.Point(448, 37);
+            this.btnSelectOriginFile.Name = "btnSelectOriginFile";
+            this.btnSelectOriginFile.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectOriginFile.TabIndex = 2;
+            this.btnSelectOriginFile.Text = "...";
+            this.btnSelectOriginFile.UseVisualStyleBackColor = true;
+            this.btnSelectOriginFile.Click += new System.EventHandler(this.BtnSelectOriginFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination Folder";
+            // 
+            // txtDestinationFolder
+            // 
+            this.txtDestinationFolder.Enabled = false;
+            this.txtDestinationFolder.Location = new System.Drawing.Point(6, 81);
+            this.txtDestinationFolder.Name = "txtDestinationFolder";
+            this.txtDestinationFolder.Size = new System.Drawing.Size(436, 23);
+            this.txtDestinationFolder.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelectDestinationFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtDestinationFolder);
+            this.groupBox1.Controls.Add(this.txtOriginFile);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnSelectOriginFile);
+            this.groupBox1.Location = new System.Drawing.Point(6, -4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(488, 126);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSelectDestinationFolder
+            // 
+            this.btnSelectDestinationFolder.Location = new System.Drawing.Point(448, 81);
+            this.btnSelectDestinationFolder.Name = "btnSelectDestinationFolder";
+            this.btnSelectDestinationFolder.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectDestinationFolder.TabIndex = 5;
+            this.btnSelectDestinationFolder.Text = "...";
+            this.btnSelectDestinationFolder.UseVisualStyleBackColor = true;
+            this.btnSelectDestinationFolder.Click += new System.EventHandler(this.BtnSelectDestinationFolder_Click);
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Location = new System.Drawing.Point(500, 12);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(75, 23);
+            this.BtnConvert.TabIndex = 6;
+            this.BtnConvert.Text = "&Convert";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(500, 41);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 7;
+            this.BtnClear.Text = "Clea&r";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // BtnInfo
+            // 
+            this.BtnInfo.Location = new System.Drawing.Point(500, 70);
+            this.BtnInfo.Name = "BtnInfo";
+            this.BtnInfo.Size = new System.Drawing.Size(75, 23);
+            this.BtnInfo.TabIndex = 8;
+            this.BtnInfo.Text = "&Info";
+            this.BtnInfo.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(500, 99);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 9;
+            this.BtnClose.Text = "Clos&e";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 129);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnInfo);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnConvert);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert your Heif images into JPG format";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtOriginFile;
+        private Button btnSelectOriginFile;
+        private Label label2;
+        private TextBox txtDestinationFolder;
+        private GroupBox groupBox1;
+        private Button btnSelectDestinationFolder;
+        private Button BtnConvert;
+        private Button BtnClear;
+        private Button BtnInfo;
+        private Button BtnClose;
+    }
+}
---
+++ HeifToJpgConverter/Form1.cs
@@ -0,0 +1,84 @@
+using ImageMagick;
+
+namespace HeifToJpgConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectOriginFile_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using var openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "heic files (*.heic)|*.heic";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                txtOriginFile.Text = filePath;
+                MessageBox.Show("File Content at path: " + filePath, "Selected File", MessageBoxButtons.OK);
+                return;
+            }
+            
+            MessageBox.Show("You need to select a file to be converted in order to proceed with the conversion", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void BtnSelectDestinationFolder_Click(object sender, EventArgs e)
+        {
+            var folderPath = string.Empty;
+
+            using var folderBrowser= new FolderBrowserDialog();
+            folderBrowser.InitialDirectory = "c:\\";
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowser.SelectedPath;
+                txtDestinationFolder.Text = folderPath;
+                MessageBox.Show("File Content at path: " + folderPath, "Selected folder", MessageBoxButtons.OK);
+                return;
+            }
+
+            MessageBox.Show("You need to select a destination folder in order to proceed with the conversion", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtDestinationFolder.Text = string.Empty;
+            txtOriginFile.Text = string.Empty;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you really want to exit?", "Close the App?", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            using var magickImage = new MagickImage(txtOriginFile.Text);
+            magickImage.Format = MagickFormat.Jpg;
+            magickImage.Write(@"" + txtDestinationFolder.Text + "\\ConvertedImage.jpg");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //txtOriginFile.Text = @"C:\Users\Joffre\OneDrive\Particular\Imagens\Imagens da Câmera\2023\02\20230217_085445749_iOS.heic";
+            //txtDestinationFolder.Text = @"C:\Users\Joffre\Desktop";
+        }
+    }
+}
